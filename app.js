736b421dbe3485c89a137ccb3f7c6d17fc8d41af
petermgoh <45682969+petermgoh@users.noreply.github.com>
2025-01-20 15:59:59 -0600

    view CSS additions

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const index = require("./routes/index")
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // for styles in public folder
 //const assetsPath = path.join(__dirname, "public");
